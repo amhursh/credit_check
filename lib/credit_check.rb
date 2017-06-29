c73322fe881d6371956e8e0e5538de3fd110d2f8
amhursh <amhursh@gmail.com>
2017-06-29 09:16:41 -0600

    Complete Credit Check Project - Aaron Hursh

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -47,3 +47,7 @@ class CreditCheck
   end
 
 end
+
+print "Please enter a credit card number that you would like to check. > "
+credit_card = CreditCheck.new(gets.chomp)
+credit_card.is_the_cc_num_valid?
